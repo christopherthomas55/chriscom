edb05a6f2541a27605afe6a9a385328350d69a7c
Christopher Thomas <christopherthomas55@gmail.com>
2017-08-05 17:56:34 -0500

    added contact

--- ChrisCom.py
+++ ChrisCom.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
+from smtplib import SMTP
+from email.mime.text import MIMEText
+
+with open('C:\\Users\\chris_000\\PycharmProjects\\ChrisCom\\emailinfo','r') as f:
+    email_info=f.readlines()
+    email_user = email_info[0]
+    email_pwd=email_info[1]
+
 app = Flask(__name__)
+app.secret_key = 'some_secret_that_you_do_not_know'
 
 
 @app.route('/')
@@ -24,6 +33,25 @@ def projects_page():
 @app.route('/resume')
 def resume_page():
     return render_template('resume_page.html')
+
+@app.route('/submit_comment',methods=['POST'])
+def submit_comment():
+    message=request.form.to_dict()
+    if message['User_Name']== '':
+        message['User_Name'] = 'Anonymous User'
+
+    s=SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.login(email_user,email_pwd)
+
+    msg=MIMEText('They sent you this message on chris.com- \n \n' +message['Comment'],'plain')
+    msg['Subject']=message['User_Name'] +' sent you a message on chris.com'
+    msg['From']=email_user
+
+    s.sendmail(email_user,email_user,msg.as_string())
+    flash('Message submitted succesfully!')
+    return redirect(url_for('contact_page'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

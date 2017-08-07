7ac5f06865833ccabbb1b1356092a3852e657795
Chris <christopherthomas55@gmail.com>
2017-08-07 18:51:43 -0500

    Fixing pi diffs

--- ChrisCom.py
+++ ChrisCom.py
@@ -20,8 +20,8 @@ app.secret_key = 'some_secret_that_you_do_not_know'
 @app.route('/home')
 def home_page():
     server_uptime = int(time()-server_start_time)
-    print(server_uptime)
-    return render_template('home_page.html', server_uptime=server_uptime)
+    #return render_template('home_page.html', server_uptime=server_uptime, meme_url=open('../static/misc/meme_url.txt','r').read())
+    return render_template('home_page.html', server_uptime=server_uptime, meme_url=open('C:/Users/chris_000/PycharmProjects/ChrisCom/static/misc/meme_url.txt','r').read())
 
 @app.route('/about')
 def about_page():
